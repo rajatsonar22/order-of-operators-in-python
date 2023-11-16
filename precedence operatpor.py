ff02287c31f100854d3ce0ca4d1bda242d7e69fc
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-11-16 13:43:45 +0530

    Add files via upload

---
+++ precedence operatpor.py
@@ -0,0 +1,14 @@
+a = 20
+b = 10
+c = 15
+d = 5
+#opreator precedence
+print(" b + a * c = ", b + a * c )
+print("( b = a ) * c = ",( b + a ) * c)
+print("( b + a ) * c = ", ( b + a ) * c )
+
+# asoscitivity of operaytor
+print("( a + b ) + c = ", ( a + b ) + c )
+print("( a + ( b + c ) = ", a +( b + c ))
+print("( a / b ) / c = ", ( a / b ) / c )
+print(" a / ( b / c ) = ", a / ( b / c ))
